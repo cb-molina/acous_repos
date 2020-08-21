db6979c6406f07aef7755433da6db679d177eefc
Christian B Molina <cbmolina@ucdavis.edu>
2020-08-20 18:14:18 -0700

    visual_1_v2 updated and visual_1_v2 visual created

--- python_source/visual_1/visual_1/visual_1/visual_1_v3.py
+++ python_source/visual_1/visual_1/visual_1/visual_1_v3.py
@@ -35,42 +35,42 @@ G = t_dependent * np.sin(k_i * X3) * np.sin(k_j * Y3) + \
 
 print("This is the data we want to get:\n\n",G[:-1, :-1, 0],"\n\n=====================================================")
 
-kvalue = 1
-ki_values = [kvalue, kvalue * 3, kvalue * 9]
-kj_values = [kvalue, kvalue * 3, kvalue * 9]
+kivalue = i * np.pi / L
+kjvalue = j * np.pi / L
+ki_values = [kivalue, kivalue * 3, kivalue * 9]
+kj_values = [kjvalue, kjvalue * 3, kjvalue * 9]
 # indexing of the ki/j_values will be done with n
 
 # We'll make v constant for now
 def omega(ki, kj, n):
-    return np.sqrt(ki ** 2 + kj ** 2) * v
+    return np.sqrt(ki[n] ** 2 + kj[n] ** 2) * v
 # This is the time dependent portion of the wave equation
 def g(t,ki,kj,n):
     return a_ij * np.cos(omega(ki,kj,n) * t)
 # This is the spatial dependent portion of the wave equation
 def f(x,y,ki,kj,n):
     return np.sin(ki[n] * x) * np.sin(kj[n] * y)
+
+n = 2
+gg = [0]
 # This is the loop I need to make to run through the k values.
-for m in 
+for m in range(0,n+1):
+    gg = gg + (g(T3, ki_values, kj_values, m) * f(X3, Y3, ki_values, kj_values, m))
+    print("This is what we have for our new value:\n\n","m is: ",m,"\n\n",gg[:-1, :-1, 0],"\n\n")
 
 
 
 # Uncommment this section once you produce the same data as G
 # ========================================================================================================
-#cax = ax.pcolormesh(x, y, G[:-1, :-1, 0], vmin=-1, vmax=1, cmap='Spectral')
-# G[:-1, :-1, 0] indicates all the 
 
+cax = ax.pcolormesh(x, y, gg[:-1, :-1, 0], vmin=-1, vmax=1, cmap='Spectral')
+fig.colorbar(cax) 
+def animate(i):
+     cax.set_array(gg[:-1, :-1, i].flatten())
+anim = animation.FuncAnimation(fig, animate, interval=150, frames=len(t)-1)
 
+writer = PillowWriter(fps=140) 
+anim.save('visual_1_v2.gif', writer=writer)
 
-#fig.colorbar(cax)
- 
-#def animate(i):
-#     cax.set_array(G[:-1, :-1, i].flatten())
-
-
-#anim = animation.FuncAnimation(fig, animate, interval=150, frames=len(t)-1)
-
-#writer = PillowWriter(fps=25) 
-##anim.save('visual_1.gif', writer=writer)
-
-#plt.draw()
-#plt.show()
+plt.draw()
+plt.show()
