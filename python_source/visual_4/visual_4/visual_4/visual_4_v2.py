d1fa7be7bfc40f0f27ba2d30305c8e865bfc82d0
Christian B Molina <cbmolina@ucdavis.edu>
2020-08-27 13:30:34 -0700

    visual_4_v2 created

---
+++ python_source/visual_4/visual_4/visual_4/visual_4_v2.py
@@ -0,0 +1,66 @@
+import numpy as np
+from numpy import pi
+import matplotlib.pyplot as plt
+from matplotlib.animation import PillowWriter
+import matplotlib.animation as animation
+
+plt.style.use('dark_background') # background style
+
+fig = plt.figure(figsize=(12,6)) # creates a figure object
+fig.set_dpi(100)            
+
+#Wave speed
+v = 1
+# k values
+k_1 = 1
+k_values = [k_1, 3*k_1, 9*k_1]
+#x axis
+x0 = np.linspace(0,pi,10000)
+#Initial time
+t0 = 0
+#Time increment
+dt = 0.05
+
+def omega(k,v):
+    return k * v
+
+# Wave Equation - time part
+def g(t,n):
+    return np.cos(omega(k_values[n],v) * t)
+# Wave Equation - spatial part
+def f(x,n):
+    return np.sin(k_values[n] * x)
+
+a = []
+
+def genWave(n):
+    global dt
+    global t0
+    global x0
+    for i in range(100):
+        for m in range(-1,n):
+            value = f(x0,n) * g(t0,n)
+        t0 = t0 + dt
+        a.append(value)
+
+genWave(0)
+genWave(1)
+genWave(2)
+
+k = 0
+def animate(i):
+    global k
+    x = a[k]
+    k += 1
+    ax1 = plt.subplot(1,2,1)    
+    ax1.clear()
+    plt.plot(x0,x,color='#87FF59')
+    plt.grid(True)
+    plt.ylim([-2,2])
+    plt.xlim([0,pi/k_1])
+    
+anim = animation.FuncAnimation(fig,animate,frames=360,interval=20)
+writer = PillowWriter(fps=25) 
+anim.save('visual_4.gif', writer=writer)
+
+plt.show()
